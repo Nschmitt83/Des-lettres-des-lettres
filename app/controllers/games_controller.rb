e8479d9a60710311cfe58f98d85993e00b7fafa8
nicolas schmitt <schmitt_953@hotmail.com>
2019-08-12 17:04:46 +0200

    exo done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     source = ('A'..'Z').to_a
@@ -8,6 +11,19 @@ class GamesController < ApplicationController
 
   def score
     console
-    raise
+    end_time = Time.now.to_i
+    time_spent = end_time - params[:start].to_i
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    word_serialized = open(url).read
+    word_checked = JSON.parse(word_serialized)
+    if word_checked["found"] == false
+      @answer = { time: time_spent, score: 0, message: "This is not an english word" }
+    elsif params[:word].upcase.split("").all? { |letter| params[:word].upcase.split("").count(letter) <= params[:grid].count(letter) }
+      @answer = { time: time_spent, score: (params[:word].length * 100 - (time_spent) * 20), message: "Well done" }
+
+    else @answer = { time: time_spent, score: 0, message: "It's not in the grid" }
+
+    end
+    # raise
   end
 end
